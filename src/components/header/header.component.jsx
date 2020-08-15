58243ba99a2f1c5c4ba463b3b17051100d3cba37
shuaibnet <abir728@gmail.com>
2020-08-15 11:53:26 +0600

    Header Added

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../asset/logo.svg';
+import './header.style.scss';
+
+
+const Header = () => (
+
+    <div className='header'>
+        <Link className='logo-container' to="/">
+            <Logo className='logo' />
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>
+                SHOP
+            </Link>
+            <Link className='option' to='/shop'>
+                CONTACT
+            </Link>
+        </div>
+
+
+
+    </div>
+
+
+);
+
+export default Header;
